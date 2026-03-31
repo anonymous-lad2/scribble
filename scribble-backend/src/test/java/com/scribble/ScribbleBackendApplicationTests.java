a122b3296c10ed178659173d5351b309bd3f4153
satyam <anonymouslad540@gmail.com>
2026-03-31 10:51:42 +0530

    backend setup

---
+++ scribble-backend/src/test/java/com/scribble/ScribbleBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.scribble;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ScribbleBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
